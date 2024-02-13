8b597f57fd82931c6a4db67b9197ea099b9fe617
NMunoz909 <munoznicholas909@gmail.com>
2024-02-12 17:25:51 -0800

    Added description as well as displaying the examples

--- Lab 2/Lab2_calc.cpp
+++ Lab 2/Lab2_calc.cpp
@@ -1,6 +1,6 @@
 //Lab2_calc.cpp 
 /***
- * {Short Descriptions of lab}
+ * {Create a program to display the difference bewteen dividing int vs int and int vs double}
 ***/ 
 
 #include <iostream>
@@ -12,6 +12,8 @@ int main() {
     int int_x = 1;
     int int_y = 2;
     double double_z = 2.0;
-    
+
+    cout << "int divided by int: " << int_x / int_y << endl; 
+    cout << "int divided by double: " << int_x / double_z << endl;
 
 }
