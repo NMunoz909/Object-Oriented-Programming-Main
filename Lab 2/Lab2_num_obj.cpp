9215dd0924fc5c6e765d66093264e621229b6541
NMunoz909 <munoznicholas909@gmail.com>
2024-02-12 16:23:23 -0800

    Save numerous files (Read description)
    LAB 1: Here I cleaned up my github desktop from my previous lab, the hello_wrld is empty for now as the original is still being graded.                       LAB 2: Save the empty files for lab2_calc as well as saving an adding the beginning outline of Lab2_num_obj

---
+++ Lab 2/Lab2_num_obj.cpp
@@ -0,0 +1,18 @@
+//Lab2_numobj.cpp
+/***
+ * {Short Description of this lab}
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    //numerical
+    int int_example = 1;
+    float float_example = 2.0;
+    double double_example = 2.0;
+    
+    
+
+}
