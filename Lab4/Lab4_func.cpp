4a95377ae1b71a35f6e01dc57a7f6cabbd31f2ad
NMunoz909 <munoznicholas909@gmail.com>
2024-02-22 14:11:50 -0800

    Fixed typo and created sumOfThree function

--- Lab4/Lab4_func.cpp
+++ Lab4/Lab4_func.cpp
@@ -3,10 +3,16 @@ using namespace std;
  
 
  /**
-  * @brief returns the sum of threer
+  * @brief returns the sum of three
   * 
   * @param x : (int)
   * @param y : (int)
   * @param z : (int)
   * @return int
  */
+
+int sumOfThree(int x, int y, int z){
+
+    return x + y + z;    
+
+}
