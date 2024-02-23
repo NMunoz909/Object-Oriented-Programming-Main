1d59c20a3473bc7f8681add524e14c097788a607
NMunoz909 <munoznicholas909@gmail.com>
2024-02-22 16:15:05 -0800

    added select function and main()

--- Lab4/Lab4_func.cpp
+++ Lab4/Lab4_func.cpp
@@ -62,4 +62,62 @@ bool isGreaterThan(double x, int y){
 * @param func_type : (string)
 * @return string
 */
+string select(string func_type){
+    string out = "";
 
+    if  (func_type == "sum"){
+        out = "The Sum is ";
+    }
+    else if (func_type == "division"){
+        out = "The quotient is ";
+    }
+    else if (func_type == "comparison"){
+        out = "The first value being greater thann the second value is";
+    }
+    else {
+        out = "Invalid function type\n";
+    }
+
+    return out;
+
+}
+
+/** myPrint
+ * @brief Calls select() to print out the output with the correct promopt
+ * 
+ * @tparam T : (int, double, or string) output of sum, division, or isGreater Than
+ * @param func_type : (string)
+ * @param output
+*/
+template <typename T>
+void myPrint(string func_type, T output){
+
+    cout << select(func_type) << output << endl;
+
+}
+
+int main()
+{
+    // initialize 3 int parameters
+    int x1 = 2;
+    int x2 = 3;
+    int x3 = 5;
+
+    // call sumOfThree
+    int sum_out = sumOfThree(x1,x2,x3);
+    // call myPrint
+    myPrint("sum",sum_out);
+
+    //initialize 2 double parameters
+    double dividend = 12345.1;
+    double divisor = 2345.6;
+
+    // call division
+    double div_out = division(dividend, divisor);
+    myPrint("division", div_out);
+
+    // compare div_out and sum_out
+    bool is_greater_out = isGreaterThan(div_out, sum_out);
+    myPrint("comparison", is_greater_out);
+    return 0;
+}
