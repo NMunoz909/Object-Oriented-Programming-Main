e53440ff7146ebf2d8596e021b9a6ae87fc311c5
NMunoz909 <munoznicholas909@gmail.com>
2024-02-28 11:48:15 -0800

    added the string header as well as the remaining private variables

--- Lab5/rpg.h
+++ Lab5/rpg.h
@@ -1,6 +1,8 @@
 #ifndef RPG_H
 #define RPG_H
 
+#include <string>
+
 using namespace std;
 
 const int SKILL_SIZE = 2;
@@ -11,12 +13,18 @@ class RPG {
         RPG();
         RPG(string name, int health, int strength, int defense, string type);
 
+
         //complete rest
 
     private: 
         //complete rest
         string type; //warrior, mage thief, archer
         string skills[SKILL_SIZE];
+        string name;
+        int health;
+        int strength;
+        int defense; 
+        
 
 
 
