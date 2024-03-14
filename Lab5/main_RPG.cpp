d24143c1f60438875c7ef72a29db20a6302c26c6
NMunoz909 <munoznicholas909@gmail.com>
2024-03-14 12:21:40 -0700

    Added the templates for the three functions to be included in main.cpp

--- Lab5/main_RPG.cpp
+++ Lab5/main_RPG.cpp
@@ -3,23 +3,74 @@
 
 using namespace std;
 
+
+/**
+ * @brief Prints both player's name and their health.
+ * Example Output for names Wiz and NPC
+ * Wiz health: 75 NPC health: 100\n
+ * 
+ * @param player1
+ * @param player2
+*/
+void displayStats(RPG player1, RPG player2){
+    //Implement the brief
+}
+
+
+/**
+ * @brief Displays who wins based on who's alive. Use and if statement to check
+ * whether player 1 is alive. If so, print that they won, else player 2 won.
+ * 
+ * Example Output:
+ * Wiz defeated NPC! Good game!\n
+ * 
+ * @param player1
+ * @param player2
+*/
+void displayEnd(RPG player1, RPG player2){
+    //Implement brief
+}
+
+
+/**
+ * @brief uses a while loop to check whether both players are alive.
+ * If so, it calls displayStats((*player1), (*player2))
+ * Inside the while loop it:
+ * Then prints the name of player one to say that it is their turn
+ * Then call (*player1).useSkill(player2)
+ * Then prints"----------------------------------\n"
+ * 
+ * Repeat the process for player 2 acting on player 1 inside the same while loop
+ * Then prints the name of player two to say that it is their
+ * then call(*player2).useSkill(player1)
+ * Then prints "---------------------------------\n"
+ * 
+ Refer to the Role Playing Game Part two instructions for the
+ example out put
+ *@param player1
+ *@param player2
+*/
+void gameLoop(RPG * player1, RPG player2){
+    //implement brief
+}
+
+
+
+
+
+
+
+
+
+
+
 int main(){
     RPG p1 = RPG("Wiz", 70, 45, 15, "mage");
     RPG p2 = RPG();
 
-    printf("%s Current Stats\n", p1.getName().c_str());
-    printf("Health: %i\t Strength: %i\t Defense: %i\n", p1.getHealth(), p1.getStrength(), p1.getDefense());
-
-    // DO THE SAME FOR p2
-    printf("%s Current Stats\n", p2.getName().c_str());
-    printf("Health: %i\t Strength: %i\t Defense: %i\n", p2.getHealth(), p2.getStrength(), p2.getDefense());
-
-    // CALL updateHealth(0) on either p1 and p2
-    p2.updateHealth(0);
-    // Print out the new health 
-    cout<< "p2 Health: " << p2.getHealth()<< "\n";
-    // CALL isAlive() on both p1 and p2
-    cout << "0 if dead, 1 if alive\n" <<"p1: "<< p1.isAlive() << "\t" << "p2: "<< p2.isAlive();
+    //gameLoop(&p1, &p2);
+    //displayEnd(p1, p2);
+    
 
     return 0;
 
