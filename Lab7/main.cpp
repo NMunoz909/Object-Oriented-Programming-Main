e5b2cad265e8235849d9e42c420be128780a75f8
NMunoz909 <munoznicholas909@gmail.com>
2024-03-21 15:58:46 -0700

    added template for the overloaded function and the array of char

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -26,6 +26,19 @@ void writeToFile(string filename, int * arr, int sample_size){
     printf("Wrote to %s\n", filename.c_str());
 }
 
+/**
+ * @brief writes sample_size number of strings
+ * Example output: if n = 2 and arr = ["cat", "dog"]
+ * cat
+ * dog
+ * @param filename
+ * @param arr
+ * @param sample_size
+*/
+void writeToFile(string filename, string * arr, int sample_size){
+
+}
+
 int main() {
     const int SAMPLE_SIZE = 10;
     int array_int[SAMPLE_SIZE] = {1,2,3,4,5,6,7,8,9,10};
@@ -33,6 +46,8 @@ int main() {
                                     "This", "is", "another", "sentence.",
                                     "The", "end"};
 
+    char array_char[SAMPLE_SIZE] = {'a', 'b', 'c', 'd', 'e', '0', '1', '2', '3', '4'};
+    
     string int_file = "array_int.csv";
     writeToFile(int_file, array_int, SAMPLE_SIZE); // arr is already a pointer
 
