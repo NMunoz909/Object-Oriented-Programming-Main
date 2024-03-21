0c59fe2bca8365980ddbb2ccd2e2b918329a67b1
NMunoz909 <munoznicholas909@gmail.com>
2024-03-21 15:04:24 -0700

    Added using namespace std and the example for on of the functions

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
+/**
+ * @brief writes sampel_size number of integers
+ * Example output: if n = 3 and arr = [1,2,3]
+ * 1
+ * 2
+ * 3
+ * 
+ * @param filename
+ * @param arr
+ * @param sample_size
+*/
+void writeToFile(string filename, int * arr, int sample_size){
+    ofstream file(filename);
+    int val;
+
+    for(int i = 0; i < sample_size; i++){
+        file << arr[i];
+        file << "\n";
+    }
+    file.close();
+    printf("Wrote to %s\n", filename.c_str());
+}
+
 int main() {
     const int SAMPLE_SIZE = 10;
     int array_int[SAMPLE_SIZE] = {1,2,3,4,5,6,7,8,9,10};
