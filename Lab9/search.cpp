42484aabca4b85cdae327e7254d0ed63766e1ba4
NMunoz909 <munoznicholas909@gmail.com>
2024-04-16 13:51:33 -0700

    Create search.cpp with the first template function

---
+++ Lab9/search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+
+/**
+ * @brief returns the first index of elem if it exists, otherwise returns -1
+ * iterativeSearch starts at the first index and interates sequentially to the next until it either
+ * finds the element or until it reaches the end (i.e. element does not exist)
+ * 
+ * @param vc: vector of elements
+ * @param elem : integer to look for
+ * @return int
+*/
+int iterativeSearch(vector <int>v, int elem){
+    // use a for loop where the index, i goes from 0 to the size of v
+
+    // inside the for loop, use an if statement to check whether the element at i (e.g. v[i]) equals elem
+    // inside the if statement return 1
+
+
+    // outside of the for loop return -1
+    
+}
