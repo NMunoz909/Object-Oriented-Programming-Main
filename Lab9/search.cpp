7b7ed892652574fda38546dcc51d4ce4215cd6c4
NMunoz909 <munoznicholas909@gmail.com>
2024-04-19 09:45:01 -0700

    Created template for binarySearch() function

--- Lab9/search.cpp
+++ Lab9/search.cpp
@@ -27,3 +27,36 @@ int iterativeSearch(vector <int>v, int elem){
     // outside of the for loop return -1
     return -1;
 }
+
+/**
+ * @brief return the index of elem, if it exists in v. Otherwise it return -1.
+ * binarySearch is recursive (i.e. function calls itself).
+ * it utilizes the fact that v is increasing order (e.g. values fo uo and
+ * duplicates are not allowed)
+ * 
+ * It calculates the mid from the start and end index. It comares v[mid] (i.e. value
+ * at mid) with elem and decides to search the left half(lower values)
+ * or right half (upper values).
+ * 
+ * @param v : vector of elements
+ * @param start : leftmost index (starting value is 0)
+ * @param end : rightmost index (starting value is v.size())
+ * @param elem : integer to look for
+ * @return int
+*/
+int binarySearch(vector<int> & v, int start, int end, int elem){
+    //write an if statement that checks the terminating case
+    //inside the if statemant return -1
+
+    // instantiate the midpoint
+
+
+    //Use if/else statements to do the following:
+    // 1) update end(search left half)
+
+    // 2) update start (search right half)
+
+    // 3) return mid (found the elem)
+
+    // return a recursice call to binarySearch(...) 
+}
