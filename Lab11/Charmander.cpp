74e475353f332f6004a6c8c731151daf9a1f24d5
NMunoz909 <munoznicholas909@gmail.com>
2024-05-08 16:51:23 -0700

    created main and fixed my charmander header file

--- Lab11/Charmander.cpp
+++ Lab11/Charmander.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include "Charmander.cpp"
+#include "Charmander.h"
 /**
  * @brief  Construct a new Charmander:: Charmander object
  * 
@@ -24,7 +24,7 @@ Charmander::Charmander() : Pokemon() {
  * @param t
 */
 Charmander::Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s)
-    Pokemon(name, hp, att, def, t){
+: Pokemon(name, hp, att, def, t){
         type = t;
         skills = s;
         cout << "Overload Constructor (Charmander)\n";
